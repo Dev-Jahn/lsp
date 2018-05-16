820ec553f7fe2739575837cd3280cac9fd7a4de5
Dev-Jahn <ajh508@naver.com>
2018-05-16 15:17:48 +0900

    sync

---
+++ b/ch6/16/ssu_signal_2.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+static void ssu_signal_handler(int signo);
+
+int main(void)
+{
+	if (signal(SIGINT, ssu_signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "cannot handle SIGINT\n");
+		exit(EXIT_FAILURE);
+	}
+	if(signal(SIGTERM, ssu_signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "cannot handler SIGTERM\n");
+		exit(EXIT_FAILURE);
+	}
+	if(signal(SIGPROF, ssu_signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "cannot handler SIGPROF\n");
+		exit(EXIT_FAILURE);
+	}
+	if(signal(SIGHUP, ssu_signal_handler) == SIG_ERR)
+	{
+		fprintf(stderr, "cannot handler SIGHUP\n");
+		exit(EXIT_FAILURE);
+	}
+	while(1)
+		pause();
+	exit(0);
+}
+static void ssu_signal_handler(int signo)
+{
+	if (signo==SIGINT)
+		printf("caught SIGINT\n");
+	else if (signo == SIGTERM)
+		printf("caught SIGTERM\n");
+	else
+	{
+		fprintf(stderr, "unexpected signal\n");
+		exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
+}
+
