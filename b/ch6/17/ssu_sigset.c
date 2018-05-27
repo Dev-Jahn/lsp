b89988fb1023ec19bbf55f18b3acb7cce96f0815
Dev-Jahn <ajh508@naver.com>
2018-05-27 20:39:04 +0900

    from laptop

---
+++ b/ch6/17/ssu_sigset.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(void)
+{
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+
+	switch (sigismember(&set, SIGINT))
+	{
+		case 1:
+			printf("SIGINT is include. \n");
+			break;
+		case 0:
+			printf("SIGINT is not include. \n");
+			break;
+		default:
+			printf("failed to call sigismember()\n");
+	}
+
+	switch (sigismember(&set, SIGSYS))
+	{
+		case 1:
+			printf("SIGSYS is included. \n");
+			break;
+		case 0:
+			printf("SIGSYS is not included. \n");
+			break;
+		default:
+			printf("failed to call sigismember() \n");
+	}
+
+	exit(0);
+}
